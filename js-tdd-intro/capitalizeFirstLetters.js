ea7073c9fc1ba7d1e847aebcf1608080c50441bd
Celine Berthelin <celine.berthelin@gmail.com>
2020-01-03 16:55:28 +0100

    test write

--- js-tdd-intro/capitalizeFirstLetters.js
+++ js-tdd-intro/capitalizeFirstLetters.js
@@ -1,19 +1,17 @@
-// capitalizeFirst.js
+// capitalizeFirstLetters.js
 const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
-function capitalizeFirst(input) {
-  return input.length > 0
-  ? input[0].toUpperCase() + input.slice(1)
-  : '';
-} 
-// Check that capitalizeFirst is a function
-assert.strictEqual(typeof capitalizeFirst, 'function');
-// Check that capitalizeFirst accepts one argument
-assert.strictEqual(capitalizeFirst.length, 1);
-// Check that capitalizeFirst transforms javaScript correctly
-assert.strictEqual(capitalizeFirst('javaScript'), 'JavaScript');
-// Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirst('z'), 'Z');
+function capitalizeFirstLetters() {
 
-assert.strictEqual(capitalizeFirst(''), '');
+}
+// Check that capitalizeFirst is a function
+assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+// Check that capitalizeFirst accepts one argument
+assert.strictEqual(capitalizeFirstLetters.length);
+// Check that capitalizeFirst transforms javaScript correctly
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
+// Check that it works for a 1-character string
+assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+// Check that it works with empty string
+assert.strictEqual(capitalizeFirstLetters(''), '');
